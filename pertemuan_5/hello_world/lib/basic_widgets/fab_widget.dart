dccc0b06ce068ef7e490d7d2d9937f8a0b9a403c
rafifrw <rafifrw@gmail.com>
2024-10-08 14:14:18 +0700

    JS5

---
+++ pertemuan_5/hello_world/lib/basic_widgets/fab_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyFabWidget extends StatelessWidget {
+  const MyFabWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () {
+        // Add your onPressed code here!
+        print("FAB Pressed");
+      },
+      child: const Icon(Icons.thumb_up),
+      backgroundColor: Colors.pink,
+    );
+  }
+}
